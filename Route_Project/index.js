46a81f9d947bb0fe158859e5142c7604e5ffcc7a
avni955587 <avni955587@gmail.com>
2026-03-30 12:09:07 +0530

    RouteProject

---
+++ Route_Project/index.js
@@ -0,0 +1,30 @@
+const express= require('express')
+const app=express();
+const reqAge=require('./middleWare')
+const router=express.Router()
+router.use(reqAge)
+
+// app.use(Age)
+app.get("",(req,res)=>{
+    res.send("WElcome to Home Page")
+})
+app.get("/about",reqAge,(req,res)=>{
+    res.send("WElcome to About Page")
+})
+app.use(router)
+router.get("/contact",(req,res)=>{
+    res.send("WElcome to Contact Page")
+})
+
+
+app.listen(3000,()=>{
+    console.log("Server is listening at http://localhost:3000")
+})
+
+
+
+
+
+
+
+
